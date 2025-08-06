fcf61ae18e9e3b7224faeda241df3b721339c323
Jeevanantham <jeevakachari@gmail.com>
2025-08-06 20:29:08 +0530

    Remove mfe-host and mfe-remote projects
    Deleted all source, configuration, and project files for both mfe-host and mfe-remote, including Angular configs, source code, and supporting files. This removes both microfrontend applications from the repository.

--- mfe-remote/src/app/remote-entry/remote-entry.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-remote-entry',
-  template: `<div class="remote-entry">Remote Entry Works!</div>`,
-  styleUrls: ['./remote-entry.component.scss']
-})
-export class RemoteEntryComponent {}
